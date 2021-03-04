4ca516bc8b76ad3d3a85b0be28f8fb57c6c87d05
Gevorg Davtyan <gevorgdavtyan99@mail.ru>
2021-03-04 21:57:54 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+
+import Header from './components/Header/header.jsx'
+
+const App = () => {
+    return (
+        <div className='text'>
+            <Header />
+            hellow from react home
+        </div>
+    )
+}
+
+export default App
